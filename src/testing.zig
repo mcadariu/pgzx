3251787385b790274c104106813be95dda33462c
Mircea Cadariu <cadariu.mircea@gmail.com>
2025-12-31 13:37:17 +0000

    fix lint issue

--- src/testing.zig
+++ src/testing.zig
@@ -12,7 +12,7 @@ comptime {
             pgzx.collections.htab.TestSuite_HTab,
 
             pgzx.meta.TestSuite_Meta,
-                // pgzx.mem.TestSuite_Mem,
+            // pgzx.mem.TestSuite_Mem,
             pgzx.node.TestSuite_Node,
         },
     );
